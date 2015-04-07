d13543ef55350f460b1ca11a779c181204bf3572
Nick Molo <nick.molo@gmail.com>
2015-04-07 12:03:18 -0400

    updated lab 3 q2 stuff

--- lab3/apps/q2/include/spawn.h
+++ lab3/apps/q2/include/spawn.h
@@ -2,10 +2,10 @@
 #define __USERPROG__
 
 
-#define FILENAME_INJECTN2 "bin/injectN2.dlx.obj"
-#define FILENAME_INJECTH2O "bin/injectH2O.dlx.obj"
-#define FILENAME_REACT1 "bin/react1.dlx.obj" 
-#define FILENAME_REACT2 "bin/react2.dlx.obj"
-#define FILENAME_REACT3 "bin/react3.dlx.obj"
+#define FILENAME_INJECTN2 "injectN2.dlx.obj"
+#define FILENAME_INJECTH2O "injectH2O.dlx.obj"
+#define FILENAME_REACT1 "react1.dlx.obj" 
+#define FILENAME_REACT2 "react2.dlx.obj"
+#define FILENAME_REACT3 "react3.dlx.obj"
 
 #endif
